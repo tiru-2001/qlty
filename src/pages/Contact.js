8cb4d40b2752375f3e9c72c35005b0def5cea49b
tiru-2001 <tirumaleshae709@gmail.com>
2022-11-23 00:44:34 +0530

    modified

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -18,7 +18,7 @@ const Contact = () => {
      <div  className="row justify-content-evenly">
       <div className="col-md-3 touchSub">
         <h3 className='text-center'>Phone</h3>
-        <h2   style={{color:" rgb(154, 78, 12)"}} className='text-center'><i class="fa-solid fa-phone"></i></h2>
+        <h2   style={{color:" rgb(154, 78, 12)"}} className='text-center'><i className="fa-solid fa-phone"></i></h2>
        <h2 className='text-center'> <a className='numbersmail' href="tel:9945305778"> 9945305778</a> </h2>
         
       </div>
@@ -32,7 +32,7 @@ const Contact = () => {
       <div className="col-md-3 touchSub">
      
         <h3 className='text-center'>Email</h3>
-        <h2   style={{color:" rgb(154, 78, 12)"}}className='text-center'><i class="fa-solid fa-envelope"></i></h2>
+        <h2   style={{color:" rgb(154, 78, 12)"}}className='text-center'><i className="fa-solid fa-envelope"></i></h2>
         <h4 className='text-center'>  <a className='numbersmail' href="mailto: qualityeggs@gmail.com">qualityeggs@gmail. <br/> com</a> </h4>
       </div>
      </div>
